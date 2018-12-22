9939b41d8225fe360754623050c949d31ed621af
arghano1 <t94B8j2Mir>
2018-12-23 00:26:55 +0330

    report daily total seconds

---
+++ pages/reports/daily_total.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mh
+ * Date: 12/17/2018
+ * Time: 15:02
+ */
+
+global $TITLE;
+$TITLE = "گزارش مجموع کارکرد روزانه";
+
+include_once 'includes/general.php';
+include_once 'includes/jdf.php';
+
+function body()
+{
+    $pdo = get_pdo();
+    $query = $pdo->query("SELECT * FROM `view_job_daily` ORDER BY `date` DESC");
+    $rows = array();
+    while ($r = $query->fetchObject()) {
+        $r->date = jdate("l d F Y", strtotime($r->date));
+        $r->work = convert_seconds($r->seconds);
+        $rows[] = $r;
+    }
+    ?>
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr class="badge-dark">
+                    <th class="text-center">تاریخ</th>
+                    <th class="text-center">زمان کار (ثانیه)</th>
+                    <th class="text-center">ساعت</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <?php foreach ($rows as $r) : ?>
+                    <tr>
+                        <td><?php echo $r->date; ?></td>
+                        <td><?php echo $r->seconds; ?></td>
+                        <td><?php echo $r->work; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?php
+}
--- template/template.php
+++ template/template.php
@@ -18,7 +18,7 @@ global  $TITLE;
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,6 +43,7 @@ global  $TITLE;
                 </a>
                 <div class="dropdown-menu" aria-labelledby="reportsDropDown">
                     <a class="dropdown-item" href="<?php echo ROOT_URL ?>/reports/daily">گزارش روزانه</a>
+                    <a class="dropdown-item" href="<?php echo ROOT_URL ?>/reports/daily_total">گزارش مجموع کارکرد روزانه</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
