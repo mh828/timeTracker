7649066f1a209ea7819cfd51c44be10cf7d7bc96
mahdi Hasanpour <mh.hasnapour@gmail.com>
2020-12-03 19:12:56 +0330

    interval report

---
+++ pages/reports/interval.php
@@ -0,0 +1,32 @@
+<?php
+global $TITLE;
+$TITLE = 'گزارش بازه‌ای';
+
+include ROOT_DIR . '/includes/jdf.php';
+
+function body()
+{
+    $start_day = jdate('d', '', '', '', 'en');
+    $start_month = jdate('m', '', '', '', 'en');
+    $start_year = jdate('Y', '', '', '', 'en');
+
+    $end_day = jdate('d', '', '', '', 'en');
+    $end_month = jdate('m', '', '', '', 'en');
+    $end_year = jdate('Y', '', '', '', 'en');
+
+    extract($_POST);
+    $start_timestamp = jmktime(0, 0, 0, $start_month, $start_day, $start_year);
+    $end_timestamp = jmktime(23, 59, 59, $end_month, $end_day, $end_year);
+
+    $pdo = get_pdo();
+    $queryString = file_get_contents(ROOT_DIR . '/queries/intervalTimeLog.sql');
+    $query = $pdo->prepare($queryString);
+    $query->bindValue(':start', $start_timestamp);
+    $query->bindValue(':end', $end_timestamp);
+    $query->execute();
+
+    var_dump($start_timestamp);
+    var_dump($end_timestamp);
+
+    include 'interval.template.php';
+}
---
+++ pages/reports/interval.template.php
@@ -0,0 +1,105 @@
+<div class="container">
+
+    <form method="post">
+
+        <div class="form-row">
+            <div class="form-group col-md">
+                <label>از تاریخ</label>
+                <div class="form-row">
+                    <div class="form-group col-sm">
+                        <label>روز شروع</label>
+                        <select class="form-control" name="start_day">
+                            <?php for ($i = 1; $i <= 31; $i++): ?>
+                                <option value="<?php echo $i; ?>" <?php echo $start_day == $i ? 'selected="selected"' : '' ?>>
+                                    <?php echo $i; ?>
+                                </option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-sm">
+                        <label>ماه شروع</label>
+                        <select class="form-control" name="start_month">
+                            <?php for ($i = 1; $i <= 12; $i++): ?>
+                                <option value="<?php echo $i; ?>" <?php echo $start_month == $i ? 'selected="selected"' : '' ?>>
+                                    <?php echo $i; ?>
+                                </option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-sm">
+                        <label>سال شروع</label>
+                        <input type="tel" pattern="[0-9]+" name="start_year" class="form-control"
+                               value="<?php echo $start_year ?>"/>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="form-group col-md">
+                <label>تا تاریخ</label>
+                <div class="form-row">
+                    <div class="form-group col-sm">
+                        <label>روز پایان</label>
+                        <select class="form-control" name="end_day">
+                            <?php for ($i = 1; $i <= 31; $i++): ?>
+                                <option value="<?php echo $i; ?>" <?php echo $end_day == $i ? 'selected="selected"' : '' ?>>
+                                    <?php echo $i; ?>
+                                </option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-sm">
+                        <label>ماه پایان</label>
+                        <select class="form-control" name="end_month">
+                            <?php for ($i = 1; $i <= 12; $i++): ?>
+                                <option value="<?php echo $i; ?>" <?php echo $end_month == $i ? 'selected="selected"' : '' ?>>
+                                    <?php echo $i; ?>
+                                </option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-sm">
+                        <label>سال پایان</label>
+                        <input type="tel" pattern="[0-9]+" name="end_year" class="form-control"
+                               value="<?php echo $end_year ?>"/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <input type="submit" class="btn btn-primary" value="دریافت گزارش"/>
+        </div>
+    </form>
+
+</div>
+
+<div class="container-fluid">
+    <div class="table-responsive">
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th>عنوان</th>
+                <th>ساعت کار انجام شده</th>
+                <th>&</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php if ($query && $query instanceof PDOStatement): ?>
+                <?php while ($row = $query->fetchObject()) { ?>
+                    <tr>
+                        <td><?php echo $row->title ?></td>
+                        <td><?php echo convert_seconds($row->sum) ?></td>
+                    </tr>
+                <?php } ?>
+            <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
