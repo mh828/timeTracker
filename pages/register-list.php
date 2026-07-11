fb897f03e35789d79a97f3299bd2217108ec2ffe
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2026-07-11 14:31:44 +0330

    open several project

--- pages/register-list.php
+++ pages/register-list.php
@@ -21,8 +21,8 @@ function body()
     $errors = array();
 
 //get on-working job
-    $on_working = $pdo->query("SELECT `time_log`.*,`job`.`title` FROM `time_log` LEFT JOIN `job` ON `job`.`job_id` = `time_log`.`job_id` WHERE IFNULL(`end`,'') = '' ORDER BY `start` DESC LIMIT 1");
-    $on_working = $on_working->fetchObject();
+    $on_working = $pdo->query("SELECT `time_log`.*,`job`.`title` FROM `time_log` LEFT JOIN `job` ON `job`.`job_id` = `time_log`.`job_id` WHERE IFNULL(`end`,'') = '' ORDER BY `start` DESC");
+    $on_working = $on_working->fetchAll(PDO::FETCH_OBJ);
 
 
     if (isset($_POST['end_job'])) {
@@ -32,8 +32,8 @@ function body()
         //$on_working->description = !empty($_POST['description']) ? $_POST['description'] : '';
 
         $stm = $pdo->prepare("UPDATE time_log  SET `end` = :end, `duration` = :duration, " .
-            "`description` = :description " .
-            "  WHERE start = :start AND job_id = :job_id;");
+                "`description` = :description " .
+                "  WHERE start = :start AND job_id = :job_id;");
         $stm->bindValue(":start", $on_working->start);
         $stm->bindValue(":end", $on_working->end);
         $stm->bindValue(":duration", $on_working->duration);
@@ -42,15 +42,14 @@ function body()
 
         if ($stm->execute())
             header("location: " . $_SERVER['REQUEST_URI']);
-    }
-    else if(isset($_POST['append_description'])){
+    } else if (isset($_POST['append_description'])) {
         $_POST = input_validate($_POST);
-        if(!empty($_POST['description'])){
+        if (!empty($_POST['description'])) {
             $query = $pdo->prepare("UPDATE time_log SET description  =  :description WHERE start = :start AND job_id = :job_id");
-            $query->bindValue(':description',trim($on_working->description . PHP_EOL . $_POST['description']) );
+            $query->bindValue(':description', trim($on_working->description . PHP_EOL . $_POST['description']));
             $query->bindValue(":start", $on_working->start);;
             $query->bindValue(":job_id", $on_working->job_id);
-            if($query->execute())
+            if ($query->execute())
                 header("location: " . $_SERVER['REQUEST_URI']);
         }
     }
@@ -77,8 +76,8 @@ function body()
 
             $stm = $pdo->prepare("INSERT INTO time_log (start,job_id) VALUES (:start,:job_id)");
             $res = $stm->execute(array(
-                ":start" => time(),
-                ":job_id" => $_POST['job_id']
+                    ":start" => time(),
+                    ":job_id" => $_POST['job_id']
             ));
 
             if ($res)
@@ -87,135 +86,135 @@ function body()
     }
 
 
-    $latestDidJobs = "select job.job_id,job.title,count(time_log.duration) as 'count',  max(time_log.end) as 'end' from job ".
-    " left join time_log on time_log.job_id = job.job_id ".
-    " where time_log.end is not null ".
-    " group by job.job_id ".
-    " order by time_log.end DESC LIMIT 5";
+    $latestDidJobs = "select job.job_id,job.title,count(time_log.duration) as 'count',  max(time_log.end) as 'end' from job " .
+            " left join time_log on time_log.job_id = job.job_id " .
+            " where time_log.end is not null " .
+            " group by job.job_id " .
+            " order by time_log.end DESC LIMIT 5";
     $latestDidJobs = $pdo->prepare($latestDidJobs);
     $latestDidJobs->execute();
     ?>
 
     <div class="container mt-2">
-<?php if (!$on_working): ?>
-    <form method="post" class="card" id="start-activity-form">
-        <h4 class="card-header">شروع فعالیت</h4>
+        <?php if (!empty($on_working)): ?>
+            <?php foreach ($on_working as $item): ?>
+                <form method="post" class="card mb-3">
+                    <input type="text" name="start" value="<?=$item->start ?>">
+                    <input type="text" name="job_id" value="<?=$item->job_id ?>">
+                    <h4 class="card-header">پایان دادن به کار در حال اجرا</h4>
 
-        <div class="card-body">
-            <div class="form-group">
-                <label>فعالیت جدید</label>
-                <div class="input-group">
-                    <input type="text" name="new_job" class="form-control" placeholder="فعالیت جدید"/>
-                    <div class="input-group-append">
-                        <input type="submit" name="start_job" value="شروع کار" class="btn btn-primary"/>
+                    <div class="card-body">
+                        <table class="table">
+                            <tr>
+                                <th>عنوان کار</th>
+                                <td><?php echo $item->title ?></td>
+                            </tr>
+                            <tr>
+                                <th>زمان شروع</th>
+                                <td><?php echo jdate("l d F Y ساعت H:i:s", $item->start, '', '', 'en'); ?></td>
+                            </tr>
+                            <tr>
+                                <th>زمان سپری شده</th>
+                                <td>
+                                    <span class="time_gone" data-start="<?= $item->start ?>"></span>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <div class="form-group">
+                            <label>توضیحات</label>
+
+                            <?php if (!empty($item->description)): ?>
+                                <div class="border p-2 rounded my-3">
+                                    <div style="white-space: pre" class="my-2"><?php echo $item->description; ?></div>
+                                </div>
+                            <?php endif; ?>
+
+                            <textarea placeholder="توضیحات" name="description" class="form-control"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <input type="submit" name="end_job" value="پایان دادن به کار" class="btn btn-primary"/>
+                        <input type="submit" name="append_description" value="اضافه کردن توضیحات" class="btn btn-info"/>
+                    </div>
+                </form>
+            <?php endforeach; ?>
+            <script>
+                setInterval(function () {
+                    document.querySelectorAll('.time_gone').forEach(i => {
+                        i.innerText = get_time(parseInt(i.dataset.start), parseInt(new Date().getTime() / 1000));
+                    })
+                }, 1000);
+
+                function get_time(start, end) {
+                    var seconds = end - start;
+                    var hours = parseInt(seconds / 3600);
+                    seconds = seconds % 3600;
+                    var minutes = parseInt(seconds / 60);
+                    seconds = seconds % 60;
+
+                    return hours + ":" + minutes + ":" + seconds;
+                }
+            </script>
+        <?php endif; ?>
+        <form method="post" class="card" id="start-activity-form">
+            <h4 class="card-header">شروع فعالیت</h4>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label>فعالیت جدید</label>
+                    <div class="input-group">
+                        <input type="text" name="new_job" class="form-control" placeholder="فعالیت جدید"/>
+                        <div class="input-group-append">
+                            <input type="submit" name="start_job" value="شروع کار" class="btn btn-primary"/>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label>انتخاب فعالیت</label>
+                <div class="form-group">
+                    <label>انتخاب فعالیت</label>
 
-                <input type="search" class="form-control" oninput="searchIn(this.value)" placeholder="جستجو"/>
-                <input type="hidden" name="job_id" value="" />
+                    <input type="search" class="form-control" oninput="searchIn(this.value)" placeholder="جستجو"/>
+                    <input type="hidden" name="job_id" value=""/>
 
-                <div class="d-flex flex-wrap" id="job-buttons">
-                    <?php foreach ($list_of_jobs as $job) : ?>
-                        <button type="button" data-title="<?php echo $job->title ?>"
-                        onclick="onJobStart(event,'<?php echo $job->job_id ?>')"
-                        class="btn btn-light m-1 shadow-sm">
-                            <?php echo $job->title ?>
-                        </button>
-                    <?php endforeach; ?>
+                    <div class="d-flex flex-wrap" id="job-buttons">
+                        <?php foreach ($list_of_jobs as $job) : ?>
+                            <button type="button" data-title="<?php echo $job->title ?>"
+                                    onclick="onJobStart(event,'<?php echo $job->job_id ?>')"
+                                    class="btn btn-light m-1 shadow-sm">
+                                <?php echo $job->title ?>
+                            </button>
+                        <?php endforeach; ?>
+                    </div>
+
+                    <hr/>
+                    <h3>آخرین کارهای انجام شده</h3>
+                    <div class="d-flex flex-column align-items-start flex-wrap" id="job-buttons">
+                        <?php while ($job = $latestDidJobs->fetchObject()) : ?>
+                            <button type="button" data-title="<?php echo $job->title ?>"
+                                    onclick="onJobStart(event,'<?php echo $job->job_id ?>')"
+                                    class="btn btn-light m-1 shadow-sm">
+                                <span><?php echo $job->title ?></span>
+                                <span class="badge badge-info ml-2"><?php echo $job->count ?></span>
+                            </button>
+                        <?php endwhile; ?>
+                    </div>
                 </div>
 
-                <hr />
-                <h3>آخرین کارهای انجام شده</h3>
-                <div class="d-flex flex-column align-items-start flex-wrap" id="job-buttons">
-                    <?php while ( $job = $latestDidJobs->fetchObject()) : ?>
-                        <button type="button" data-title="<?php echo $job->title ?>"
-                        onclick="onJobStart(event,'<?php echo $job->job_id ?>')"
-                        class="btn btn-light m-1 shadow-sm">
-                            <span><?php echo $job->title ?></span>
-                            <span class="badge badge-info ml-2"><?php echo $job->count ?></span>
-                        </button>
-                    <?php endwhile; ?>
-                </div>
+                <ul class="text-danger">
+                    <?php foreach ($errors as $error) {
+                        echo "<li>{$error}</li>";
+                    } ?>
+                </ul>
+
             </div>
 
-            <ul class="text-danger">
-                <?php foreach ($errors as $error) {
-                    echo "<li>{$error}</li>";
-                } ?>
-            </ul>
+            <div class="card-footer">
 
-        </div>
-
-        <div class="card-footer">
-
-        </div>
-    </form>
-<?php else: ?>
-    <form method="post" class="card">
-        <h4 class="card-header">پایان دادن به کار در حال اجرا</h4>
-
-        <div class="card-body">
-            <table class="table">
-                <tr>
-                    <th>عنوان کار</th>
-                    <td><?php echo $on_working->title ?></td>
-                </tr>
-                <tr>
-                    <th>زمان شروع</th>
-                    <td><?php echo jdate("l d F Y ساعت H:i:s", $on_working->start, '', '', 'en'); ?></td>
-                </tr>
-                <tr>
-                    <th>زمان سپری شده</th>
-                    <td>
-                        <span id="time_gone"></span>
-                    </td>
-                </tr>
-            </table>
-
-            <div class="form-group">
-                <label>توضیحات</label>
-
-                <?php if(!empty($on_working->description)): ?>
-                <div class="border p-2 rounded my-3">
-                    <div style="white-space: pre" class="my-2"><?php echo $on_working->description; ?></div>
-                </div>
-                <?php endif; ?>
-
-                <textarea placeholder="توضیحات" name="description" class="form-control"></textarea>
             </div>
-        </div>
-
-        <div class="card-footer">
-            <input type="submit" name="end_job" value="پایان دادن به کار" class="btn btn-primary"/>
-            <input type="submit" name="append_description" value="اضافه کردن توضیحات" class="btn btn-info"/>
-        </div>
-    </form>
-
-    <script>
-        var s = <?php echo $on_working->start ?>;
-
-        setInterval(function () {
-            var e = parseInt(new Date().getTime() / 1000);
-            $("#time_gone").text(get_time(s, e));
-        }, 1000);
-
-        function get_time(start, end) {
-            var seconds = end - start;
-            var hours = parseInt(seconds / 3600);
-            seconds = seconds % 3600;
-            var minutes = parseInt(seconds / 60);
-            seconds = seconds % 60;
-
-            return hours + ":" + minutes + ":" + seconds;
-        }
-    </script>
-<?php endif; ?>
-
-
+        </form>
+    </div>
     <?php
 }
 
@@ -223,21 +222,21 @@ function script()
 {
     ?>
     <script>
-    function searchIn(value){
-        $('#job-buttons button[data-title]').each((i,e)=>{
-            if($(e).attr('data-title').startsWith(value))
-                $(e).show();
-            else
-                $(e).hide();
-        })
-    }
+        function searchIn(value) {
+            $('#job-buttons button[data-title]').each((i, e) => {
+                if ($(e).attr('data-title').startsWith(value))
+                    $(e).show();
+                else
+                    $(e).hide();
+            })
+        }
 
-    function onJobStart(e,job_id){
-        const form = document.getElementById('start-activity-form');
-        form.job_id.value = job_id;
-        $(form).append('<input type="hidden" name="start_job" />');
-        form.submit();
-    }
+        function onJobStart(e, job_id) {
+            const form = document.getElementById('start-activity-form');
+            form.job_id.value = job_id;
+            $(form).append('<input type="hidden" name="start_job" />');
+            form.submit();
+        }
     </script>
     <?php
 }
